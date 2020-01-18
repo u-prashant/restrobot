f7712d8736ab94c4a0bb1fc9b507cf41e8bc3289
Prashant U <prashant.upadhyay@privafy.com>
2020-01-18 16:16:02 +0530

    added more cuisines

--- actions.py
+++ actions.py
@@ -35,7 +35,7 @@ class ActionSearchRestaurants(Action):
             for restaurant in d['restaurants']:
                 response = response + "Found " + restaurant['restaurant']['name'] + " in " + \
                            restaurant['restaurant']['location']['address'] + \
-                           "has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
+                           " has been rated " + restaurant['restaurant']['user_rating']['aggregate_rating'] + "\n"
 
         dispatcher.utter_message("-----" + response)
         return [SlotSet('location', loc)]
